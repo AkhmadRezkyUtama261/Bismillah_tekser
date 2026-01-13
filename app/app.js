68c3305342d76054d5e7a4ebeb93c0985fd08168
Akhmad Rezky Utama <ahkmadrezkyutama369@gmail.com>
2026-01-13 18:22:09 +0700

    feat : update route

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
+require("dotenv").config;
 const express = require("express");
 const path = require("path");
 const routes = require("./routes");
 
 const app = express();
+const PORT = process.env.APP_PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,6 +14,6 @@ app.set("views", path.join(__dirname, "view"));
 
 app.use("/", routes);
 
-app.listen(3000, () => {
-  console.log("App running on port 3000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`App running on port ${PORT}`);
 });
